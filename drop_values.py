378baa2cc5977f1625358cb41ac9f16db5927bcf
Mindaugas Krasauskas <mindaugas@MindaugassMBP2.lan>
2019-11-17 14:37:39 +0200

    added class_weights

--- drop_values.py
+++ drop_values.py
@@ -1,4 +1,7 @@
 
+import numpy as np
+
+
 def drop_values(df, col_name, values):
     return df.loc[~df[col_name].isin(values), :].reset_index(drop=True)
 
@@ -8,3 +11,12 @@ def drop_rare_values(df, col_name, threshold):
     return df.loc[df[col_name].isin(counts[counts > threshold].index), :].reset_index(
         drop=True
     )
+
+
+class_weights = dict(
+    enumerate(
+        sk.utils.class_weight.compute_class_weight(
+            "balanced", np.unique(data_container.train.y), data_container.train.y
+        )
+    )
+)
