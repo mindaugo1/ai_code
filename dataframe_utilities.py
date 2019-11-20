3efb45e437be8c481aeb67a3ff02213276dc17a6
Mindaugas Krasauskas <mindaugas@MindaugassMBP2.lan>
2019-11-20 18:53:02 +0200

    minor changes

--- dataframe_utilities.py
+++ dataframe_utilities.py
@@ -20,20 +20,17 @@ class TransformingDataframes:
             not_null_values += not_null
         print(not_null_values)
 
+    def drop_values_from_dataframe(dataframe, col_name, values_to_drop):
+        return dataframe.loc[~dataframe[col_name].isin(values_to_drop), :].reset_index(drop=True)
 
-def drop_values_from_dataframe(dataframe, col_name, values_to_drop):
-    return dataframe.loc[~dataframe[col_name].isin(values_to_drop), :].reset_index(drop=True)
+    def drop_rare_values_from_dataframe(dataframe, col_name, threshold):
+        counts = dataframe[col_name].value_counts(normalize=True)
+        return dataframe.loc[dataframe[col_name].isin(counts[counts > threshold].index), :].reset_index(drop=True)
 
-
-def drop_rare_values_from_dataframe(dataframe, col_name, threshold):
-    counts = dataframe[col_name].value_counts(normalize=True)
-    return dataframe.loc[dataframe[col_name].isin(counts[counts > threshold].index), :].reset_index(drop=True)
-
-
-class_weights = dict(
-    enumerate(
-        sk.utils.class_weight.compute_class_weight(
-            "balanced", np.unique(data_container.train.y), data_container.train.y
+    class_weights = dict(
+        enumerate(
+            sk.utils.class_weight.compute_class_weight(
+                "balanced", np.unique(data_container.train.y), data_container.train.y
+            )
         )
     )
-)
