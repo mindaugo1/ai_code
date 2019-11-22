9008307c6cb353808933ba5d12a92c0105e6a341
Mindaugas Krasauskas <mindaugas@MindaugassMBP2.lan>
2019-11-22 16:15:02 +0200

    added return statement

--- TfUtils.py
+++ TfUtils.py
@@ -6,6 +6,7 @@ def create_tf_dataset_from_data_bundle_obj(data_bundle):
     y_dataset = tf.data.Dataset.from_tensor_slices(data_bundle.y)
     return tf.data.Dataset.zip((x_dataset, y_dataset))
 
+
 def make_one_hot_tf_dataset_from_sequence_data(data, vocabulary_size, window_length, batch_size):
     dataset = tf.data.Dataset.from_tensor_slices(data)
     dataset = dataset.repeat().window(window_length, shift=1, drop_remainder=True)
@@ -13,3 +14,4 @@ def make_one_hot_tf_dataset_from_sequence_data(data, vocabulary_size, window_len
     dataset = dataset.shuffle(len(data)//100).batch(batch_size)
     dataset = dataset.map(lambda windows: (windows[:, :-1], windows[:, 1:]))
     dataset = dataset.map(lambda xs, ys: (tf.one_hot(xs, depth=vocabulary_size), ys))
+    return dataset
