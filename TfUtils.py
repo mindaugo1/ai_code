123a91be2cebb7d6cf69cb417b5e5b7b47f735db
Mindaugas Krasauskas <mindaugas@MindaugassMBP2.lan>
2019-11-22 16:14:02 +0200

    removed class declaration

--- TfUtils.py
+++ TfUtils.py
@@ -1,19 +1,15 @@
 import tensorflow as tf
 
 
-class ToTfUtils:
+def create_tf_dataset_from_data_bundle_obj(data_bundle):
+    x_dataset = tf.data.Dataset.from_tensor_slices(data_bundle.x)
+    y_dataset = tf.data.Dataset.from_tensor_slices(data_bundle.y)
+    return tf.data.Dataset.zip((x_dataset, y_dataset))
 
-    @staticmethod
-    def create_tf_dataset_from_data_bundle_obj(data_bundle):
-        x_dataset = tf.data.Dataset.from_tensor_slices(data_bundle.x)
-        y_dataset = tf.data.Dataset.from_tensor_slices(data_bundle.y)
-        return tf.data.Dataset.zip((x_dataset, y_dataset))
-
-    @staticmethod
-    def make_one_hot_tf_dataset_from_sequence_data(data, vocabulary_size, window_length, batch_size):
-        dataset = tf.data.Dataset.from_tensor_slices(data)
-        dataset = dataset.repeat().window(window_length, shift=1, drop_remainder=True)
-        dataset = dataset.flat_map(lambda window: window.batch(window_length))
-        dataset = dataset.shuffle(len(data)//100).batch(batch_size)
-        dataset = dataset.map(lambda windows: (windows[:, :-1], windows[:, 1:]))
-        dataset = dataset.map(lambda xs, ys: (tf.one_hot(xs, depth=vocabulary_size), ys))
+def make_one_hot_tf_dataset_from_sequence_data(data, vocabulary_size, window_length, batch_size):
+    dataset = tf.data.Dataset.from_tensor_slices(data)
+    dataset = dataset.repeat().window(window_length, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda window: window.batch(window_length))
+    dataset = dataset.shuffle(len(data)//100).batch(batch_size)
+    dataset = dataset.map(lambda windows: (windows[:, :-1], windows[:, 1:]))
+    dataset = dataset.map(lambda xs, ys: (tf.one_hot(xs, depth=vocabulary_size), ys))
