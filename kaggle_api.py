a3257358e9b75bfd1902c51f5831bb8cf6acf2c5
Mindaugas Krasauskas <mindaugas@MindaugassMBP2.lan>
2020-04-18 19:37:01 +0300

    added correct commented code

--- kaggle_api.py
+++ kaggle_api.py
@@ -1,14 +1,14 @@
-DOWNLOAD_DATA = True
-if DOWNLOAD_DATA:
-    !rm - rf data
-    !rm - fr temp
-    from google.colab import drive
-    drive.mount('/content/drive')
-    !mkdir .kaggle/
-    !cp ./drive/My\ Drive/'Colab Notebooks'/kaggle/kaggle.json / root/.kaggle/
-    !chmod 600 / root/.kaggle/kaggle.json
-    DATA_DIR.mkdir(parents=True, exist_ok=True)
-    TEMP_DIR.mkdir(parents=True, exist_ok=True)
-    import kaggle
-    kaggle.api.authenticate()
-    kaggle.api.dataset_download_files('jrobischon/wikipedia-movie-plots', path=DATA_DIR, unzip=True)
+# DOWNLOAD_DATA = True
+# if DOWNLOAD_DATA:
+#     shutil.rmtree(str(DATA_DIR))
+#     shutil.rmtree(str(TEMP_DIR))
+#     from google.colab import drive
+#     drive.mount('/content/drive')
+#     !mkdir .kaggle/
+#     !cp /content/drive/My\ Drive/Colab\ Notebooks/kaggle/kaggle.json /content/.kaggle
+#     !chmod 600 /content/.kaggle/kaggle.json
+#     DATA_DIR.mkdir(parents=True, exist_ok=True)
+#     TEMP_DIR.mkdir(parents=True, exist_ok=True)
+#     import kaggle
+#     kaggle.api.authenticate()
+#     kaggle.api.dataset_download_files('jrobischon/wikipedia-movie-plots', path=DATA_DIR, unzip=True)
